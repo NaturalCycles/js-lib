921775a5c1fcf1919edc91c55c2266486aff6b3d
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2018-10-13 20:21:10 +0200

    fix

--- src/bin/update-from-shared-module.js
+++ src/bin/update-from-shared-module.js
@@ -4,4 +4,4 @@ const cpx = require('cpx')
 
 const overwriteDir = `./node_modules/@naturalcycles/shared-module/cfg/overwrite`
 
-cpx.copy(`${overwriteDir}/**/*`, './')
+cpx.copy(`${overwriteDir}/**/{*,.*}`, './')
