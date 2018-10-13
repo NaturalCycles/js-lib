8cdbc2c6df59456adbd7e2eac5c55a973d7895e4
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2018-10-13 17:35:54 +0200

    progress

--- src/util/exec.util.js
+++ src/util/exec.util.js
@@ -1,14 +1,22 @@
 const { spawn, exec, execSync } = require('child_process')
 
 module.exports.execCommand = async (cmd, exitOnError = true) => {
-  return new Promise(resolve => {
-    const cp = spawn(cmd, {shell: true})
-    cp.stdout.on('data', data => console.log(data.toString()))
-    cp.stderr.on('data', data => console.log(data.toString()))
-    cp.on('close', code => {
+  return new Promise((resolve, reject) => {
+    const cp = spawn(cmd, {shell: true, stdio: 'inherit'})
+    // cp.stdout.on('data', data => console.log(data.toString()))
+    // cp.stderr.on('data', data => console.log(data.toString()))
+    cp.once('error', err => reject(err))
+    cp.once('close', code => {
       // console.log('close: ' + code)
-      if (code && exitOnError) process.exit(code)
-      resolve(code)
+      if (code) {
+        if (exitOnError) {
+          process.exit(code)
+        } else {
+          reject(new Error(`${cmd} exitCode: ${code}`))
+        }
+      } else {
+        resolve(code)
+      }
     })
   })
 
