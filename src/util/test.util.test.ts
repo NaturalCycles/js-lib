825addf963635428c5c1719261f4161442eb5042
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2018-10-27 03:58:11 +0200

    fix

--- src/util/test.util.test.ts
+++ src/util/test.util.test.ts
@@ -3,3 +3,10 @@ import { getFullICUPathIfExists } from './test.util'
 test('getFullICUPathIfExists', () => {
   expect(getFullICUPathIfExists()).toBeUndefined()
 })
+
+test('test run', async () => {
+  const fullICUPath = '/Users/aaa/node_modules/full-icu'
+
+  const cmd = [fullICUPath && `NODE_ICU_DATA=${fullICUPath}`, 'jest'].filter(t => t).join(' ')
+  expect(cmd).toBe('NODE_ICU_DATA=/Users/aaa/node_modules/full-icu jest')
+})
