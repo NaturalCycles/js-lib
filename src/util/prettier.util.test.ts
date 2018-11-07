900e36c9bfe5ea89974063c37852752ad8bbac6c
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2018-11-07 16:12:26 +0100

    fix: fix prettier.util.test

--- src/util/prettier.util.test.ts
+++ src/util/prettier.util.test.ts
@@ -1,28 +1,29 @@
-import { cfgDir } from '../cnst/paths.cnts'
+import { srcDir } from '../cnst/paths.cnts'
 import { execCommand } from './exec.util'
 import { runPrettier } from './prettier.util'
 
 // const asMock = <T>(a: T): jest.Mock<T> => a as any
 
-test('runPrettier, has config', async () => {
+test('runPrettier, shared config', async () => {
   const _execCommand = ((execCommand as any) = jest.fn())
-  process.cwd = jest.fn(() => `${cfgDir}`)
+  // mock cwd, so config is not found
+  process.cwd = jest.fn(() => __dirname)
   await runPrettier()
   // expect(_execCommand).toHaveBeenCalled()
   const cmd = _execCommand.mock.calls[0][0]
   // console.log(cmd)
   expect(cmd).toMatch('prettier')
-  expect(cmd).not.toMatch('--config')
+  expect(cmd).toMatch('shared-module/cfg/prettier.config.js')
 })
 
-test('runPrettier, no config', async () => {
+test('runPrettier, local config', async () => {
   const _execCommand = ((execCommand as any) = jest.fn())
-  // mock cwd, so config is not found
-  process.cwd = jest.fn(() => __dirname)
+  // mock cwd, so config is found
+  process.cwd = jest.fn(() => `${srcDir}/test/integration/prettier`)
 
   await runPrettier()
   const cmd = _execCommand.mock.calls[0][0]
-  // console.log(cmd)
+  console.log(cmd)
   expect(cmd).toMatch('prettier')
-  expect(cmd).toMatch('--config')
+  expect(cmd).toMatch('test/integration/prettier/prettier.config.js')
 })
