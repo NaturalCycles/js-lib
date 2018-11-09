e35f747bafb88331a1864783b7679a5a73a6bafb
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2018-11-09 21:09:31 +0100

    feat: transpiling
    Preparing library to be consumed without @linked

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { memo } from './decorators/memo.decorator'
+import { memoCache } from './decorators/memoCache.decorator'
+import { AppError } from './error/app.error'
+import {
+  deepFreeze,
+  runAllTests,
+  silentConsole,
+  silentConsoleIfRunAll,
+} from './testing/test.shared.util'
+import { PromiseMap, StringMap } from './types'
+import { objectUtil } from './util/object.util'
+import { randomSharedUtil } from './util/random.shared.util'
+import { scriptSharedUtil } from './util/script.shared.util'
+import { stringSharedUtil } from './util/string.shared.util'
+
+export {
+  memo,
+  memoCache,
+  AppError,
+  deepFreeze,
+  silentConsole,
+  runAllTests,
+  silentConsoleIfRunAll,
+  objectUtil,
+  randomSharedUtil,
+  scriptSharedUtil,
+  stringSharedUtil,
+  StringMap,
+  PromiseMap,
+}
