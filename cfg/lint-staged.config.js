9ff48c1fb4b0d2a39f53140d18493a3215bd2239
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2018-11-29 15:59:28 +0100

    fix: prettier in git hook
    Wasn't working cause glob pattern like `'./src/**/*.ts,!./src/scripts/**/*'` was wrong - not supported by `lint-staged`, unfortunately.

--- cfg/lint-staged.config.js
+++ cfg/lint-staged.config.js
@@ -27,24 +27,16 @@ const prettierExtensionsExceptTs = `css,scss,js,json,md,graphql,yml,yaml,html`
 module.exports = {
   linters: {
     // For *.ts files we run first Prettier, then TSLint
-    // `-p tsconfig.json` is disabled due to extreme slowness, will be done in ci `lint-job` instead
-    // './src/**/*.ts': ['prettier --write', 'tslint -p tsconfig.json -t stylish --fix', 'git add'],
     // There are 2 tslint tasks, one without `-p` and the second is with `-p` - it is a speed optimization
-    // First: run both tslints, not in scripts
-    './src/**/*.ts,!./src/scripts/**/*': [
-      prettierCmd,
-      tslintCmd,
-      `${tslintCmd} -p tsconfig.json`,
-      'git add',
-    ],
-    // Second: run in scripts folder
-    './src/scripts/**/*.ts': [prettierCmd, tslintCmd, 'git add'],
+    './src/**/*.ts': [prettierCmd, tslintCmd, `${tslintCmd} -p tsconfig.json`, 'git add'],
 
     // For all other files we run only Prettier (because e.g TSLint screws *.scss files)
     // Everything inside `/src`
     [`./{src,doc,cfg,.circleci}/**/*.{${prettierExtensionsExceptTs}}`]: [prettierCmd, 'git add'],
 
     // Files in root dir
-    [`./*.{${prettierExtensionsExceptTs}},!./CHANGELOG.md`]: [prettierCmd, 'git add'],
+    [`./*.{${prettierExtensionsExceptTs}}`]: [prettierCmd, 'git add'],
   },
+
+  ignore: ['./src/scripts/**/*', './CHANGELOG.md'],
 }
