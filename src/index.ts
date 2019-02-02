aed1a382245fa54ef64a6d5438147b2fba99e7ee
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-02-02 16:27:36 +0100

    fix: export error classes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { memo } from './decorators/memo.decorator'
 import { memoCache } from './decorators/memoCache.decorator'
 import { AppError } from './error/app.error'
+import { ErrorData } from './error/error.model'
+import { Error400 } from './error/error400'
+import { Error401 } from './error/error401'
+import { Error401Admin } from './error/error401admin'
+import { Error403 } from './error/error403'
+import { Error403Admin } from './error/error403admin'
+import { Error404 } from './error/error404'
+import { Error409 } from './error/error409'
+import { Error500 } from './error/error500'
 import {
   deepFreeze,
   runAllTests,
@@ -16,7 +25,16 @@ import { stringSharedUtil } from './util/string.shared.util'
 export {
   memo,
   memoCache,
+  ErrorData,
   AppError,
+  Error400,
+  Error401,
+  Error401Admin,
+  Error403,
+  Error403Admin,
+  Error404,
+  Error409,
+  Error500,
   deepFreeze,
   silentConsole,
   runAllTests,
