2735e5e0ce88ecf693f68eac15f5dc7b32b61503
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-02-10 03:08:12 +0100

    fix: export ErrorObject

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { memo } from './decorators/memo.decorator'
 import { memoCache } from './decorators/memoCache.decorator'
 import { AppError } from './error/app.error'
-import { ErrorData, ErrorResponse, HttpErrorData } from './error/error.model'
+import { ErrorData, ErrorObject, ErrorResponse, HttpErrorData } from './error/error.model'
 import { HttpError } from './error/http.error'
 import { BasicLogFunction, LOG_LEVEL, LogFunction } from './log/log.shared.model'
 import {
@@ -21,6 +21,7 @@ export {
   memo,
   memoCache,
   ErrorData,
+  ErrorObject,
   HttpErrorData,
   ErrorResponse,
   AppError,
