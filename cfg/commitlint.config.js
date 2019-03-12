9c06e17a7a7418c5408ba357de84e3d0d0b20445
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-03-12 11:53:16 +0100

    fix(commitlint): ALLOW subject-case to be 'start-case'
    To allow, e.g:
    feat: POST /qa/smth

--- cfg/commitlint.config.js
+++ cfg/commitlint.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Overriding to allow 'start-case', 'upper-case', 'sentense-case', 'pascal-case'
+    // Example to allow now (which weren't allowed before):
+    // feat: POST /qa/smth
+    'subject-case': [0],
+  },
 }
