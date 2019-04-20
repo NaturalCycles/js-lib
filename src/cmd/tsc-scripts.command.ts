4cfe02cbcea135adac327d03615e9c99ab47c314
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-04-20 23:42:24 +0200

    fix: skip tsc-scripts if ./scripts doesn't exist

--- src/cmd/tsc-scripts.command.ts
+++ src/cmd/tsc-scripts.command.ts
@@ -1,7 +1,13 @@
+import * as fs from 'fs-extra'
 import { execCommand } from '../util/exec.util'
 import { ensureProjectTsconfigScripts } from '../util/tsc.util'
 
 export async function tscScriptsCommand (): Promise<void> {
+  if (!fs.existsSync('./scripts')) {
+    // ./scripts folder doesn't exist, skipping
+    return
+  }
+
   const projectTsconfigPath = await ensureProjectTsconfigScripts()
 
   const args: string[] = ['-P', projectTsconfigPath, '--noEmit']
