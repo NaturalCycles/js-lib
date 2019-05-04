ecc3a8ac86ad7ffe8f69f7660c964eb1d631147d
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-05-04 21:17:00 +0200

    chore: chore

--- src/util/test.util.ts
+++ src/util/test.util.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs-extra'
  * Returns true if module with given name exists in _target project's_ node_modules.
  */
 export function nodeModuleExists (moduleName: string): boolean {
-  return fs.pathExistsSync(`${process.cwd()}/node_modules/${moduleName}`)
+  return fs.pathExistsSync(`./node_modules/${moduleName}`)
 }
 
 export function getFullICUPathIfExists (): string | undefined {
