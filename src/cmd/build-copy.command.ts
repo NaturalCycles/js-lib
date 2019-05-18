e557c1fab1aef91f1246ed71af6ab49e76d49699
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-05-19 01:46:09 +0200

    feat: build-copy to copy *.js files
    To allow to use *.js modules which otherwise won't be transpiled/copied to dist by tsc.

--- src/cmd/build-copy.command.ts
+++ src/cmd/build-copy.command.ts
@@ -2,7 +2,7 @@ import { kpy } from '@naturalcycles/fs-lib'
 
 export async function buildCopyCommand (): Promise<void> {
   const baseDir = 'src'
-  const inputPatterns = ['**', '!**/*.{ts,js}', '!**/__snapshots__', '**/__exclude', '!test']
+  const inputPatterns = ['**', '!**/*.ts', '!**/__snapshots__', '**/__exclude', '!test']
   const outputDir = 'dist'
 
   await kpy({
