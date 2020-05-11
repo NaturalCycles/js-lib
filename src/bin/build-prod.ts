af470de849b1ccc4d5242cfa8fbab81c513c4e83
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2020-05-11 10:50:09 +0200

    fix: buildProdCommand was used by backend-lib

--- src/bin/build-prod.ts
+++ src/bin/build-prod.ts
@@ -1,12 +1,6 @@
 #!/usr/bin/env node
 
 import { runScript } from '@naturalcycles/nodejs-lib/dist/script'
-import * as fs from 'fs-extra'
-import { buildCopyCommand } from '../cmd/build-copy.command'
-import { tscProdCommand } from '../cmd/tsc-prod.command'
+import { buildProdCommand } from '../cmd/build-prod.command'
 
-runScript(async () => {
-  await fs.emptyDir('./dist')
-  await buildCopyCommand()
-  await tscProdCommand()
-})
+runScript(buildProdCommand)
