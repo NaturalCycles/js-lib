a16aefb5207b9f52d938810bc76809c3aa0acce9
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2020-08-10 18:40:22 +0200

    feat: _range to support 1-arg and 3-arg usage, aligned with lodash

--- src/array/range.test.ts
+++ src/array/range.test.ts
@@ -1,6 +1,10 @@
 import { _range } from './range'
 
 test('_range', () => {
+  expect(_range(3)).toEqual([0, 1, 2])
   expect(_range(3, 6)).toEqual([3, 4, 5])
   expect(_range(6, 3)).toEqual([])
+  expect(_range(1, 10, 2)).toEqual([1, 3, 5, 7, 9])
+  expect(_range(1, 11, 2)).toEqual([1, 3, 5, 7, 9])
+  expect(_range(1, 12, 2)).toEqual([1, 3, 5, 7, 9, 11])
 })
--- src/array/range.ts
+++ src/array/range.ts
@@ -4,8 +4,17 @@
  * Right bound is Exclusive (not Inclusive), to comply with lodash _.range
  *
  * @example
+ * range(3) // [0, 1, 2]
  * range(3, 6) // [ 3, 4, 5 ]
+ * range(1, 10, 2) // [ 1, 3, 5, 7, 9 ]
  */
-export function _range(fromIncl: number, toExcl: number): number[] {
-  return Array.from({ length: toExcl - fromIncl }, (_v, k) => k + fromIncl)
+export function _range(fromIncl: number, toExcl?: number, step = 1): number[] {
+  if (toExcl === undefined) {
+    return Array.from(Array(fromIncl), (_, i) => i)
+  }
+
+  return Array.from(
+    { length: Math.ceil((toExcl - fromIncl) / step) },
+    (_, i) => i * step + fromIncl,
+  )
 }
