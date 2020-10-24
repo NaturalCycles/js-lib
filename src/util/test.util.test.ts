4f1a0acab065f8f91d19687d5cec54a88391cd83
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2020-10-24 22:46:07 +0200

    feat: mockAllKindsOfThings extended with ErrorWithCode

--- src/util/test.util.test.ts
+++ src/util/test.util.test.ts
@@ -4,9 +4,9 @@ test('getFullICUPathIfExists', () => {
   expect(getFullICUPathIfExists()).toBeUndefined()
 })
 
-test('test run', async () => {
+test('test run', () => {
   const fullICUPath = '/Users/aaa/node_modules/full-icu'
 
-  const cmd = [fullICUPath && `NODE_ICU_DATA=${fullICUPath}`, 'jest'].filter(t => t).join(' ')
+  const cmd = [fullICUPath && `NODE_ICU_DATA=${fullICUPath}`, 'jest'].filter(Boolean).join(' ')
   expect(cmd).toBe('NODE_ICU_DATA=/Users/aaa/node_modules/full-icu jest')
 })
