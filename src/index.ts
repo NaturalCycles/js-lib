750cbbb169d70dc3f9febc109b1f31565b04a8a9
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2020-11-23 17:25:05 +0100

    feat: export git* api from git.util (still experimental)

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import { BuildInfo } from './util/buildInfo.model'
 import { generateBuildInfo } from './util/buildInfo.util'
 
 export { BuildInfo, generateBuildInfo, buildProdCommand }
+
+export * from './util/git.util'
