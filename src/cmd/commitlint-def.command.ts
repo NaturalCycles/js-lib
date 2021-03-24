6a399081f17e52d86f7198476b040e46c2556379
Kristofer Borgström <kristofer@libertar.se>
2021-03-24 11:30:35 +0100

    feat(ci): add branch env var to commitlint execution

--- src/cmd/commitlint-def.command.ts
+++ src/cmd/commitlint-def.command.ts
@@ -1,6 +1,7 @@
 import { execWithArgs } from '@naturalcycles/nodejs-lib/dist/exec'
 import * as fs from 'fs'
 import { cfgDir } from '../cnst/paths.cnst'
+import { gitCurrentBranchName } from '../util/git.util'
 
 export async function commitlintDefCommand(): Promise<void> {
   const cwd = process.cwd()
@@ -8,5 +9,6 @@ export async function commitlintDefCommand(): Promise<void> {
   const sharedConfig = `${cfgDir}/commitlint.config.js`
   const config = fs.existsSync(localConfig) ? localConfig : sharedConfig
 
-  await execWithArgs(`commitlint`, [`-E`, `HUSKY_GIT_PARAMS`, `--config`, config])
+  const env = { GIT_BRANCH: await gitCurrentBranchName() }
+  await execWithArgs(`commitlint`, [`-E`, `HUSKY_GIT_PARAMS`, `--config`, config], { env })
 }
