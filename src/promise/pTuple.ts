b664f89a5fea8056963bbdacf4ef117eb9597de3
Ignacio Cabrera <cabrerabywaters@gmail.com>
2021-03-26 12:17:43 +0100

    feat(): change commit message to trigger CI

--- src/promise/pTuple.ts
+++ src/promise/pTuple.ts
@@ -1,10 +1,8 @@
 /**
- *
  * Wraps async calls in try catch blocks
  * to simplify syntax.
  *
  * source: https://github.com/scopsy/await-to-js/blob/master/src/await-to-js.ts
- *
  */
 export async function pTuple<T, U = Error>(
   promise: Promise<T>,
