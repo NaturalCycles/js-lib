3393b70e57f9b8bc14756fc1780c91a2e06af5b8
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2021-04-01 14:54:41 +0200

    test: increase coverage

--- src/types.test.ts
+++ src/types.test.ts
@@ -1,5 +1,16 @@
-import { Reviver } from './types'
+import {
+  Reviver,
+  _passNothingPredicate,
+  _passthroughMapper,
+  _passthroughPredicate,
+  _passUndefinedMapper,
+} from './types'
 
 test('types', () => {
   const _reviver: Reviver = (k: string, v: string) => {}
+
+  expect(_passthroughMapper('a', 1)).toBe('a')
+  expect(_passUndefinedMapper('a', 1)).toBeUndefined()
+  expect(_passthroughPredicate('a', 1)).toBe(true)
+  expect(_passNothingPredicate('a', 1)).toBe(false)
 })
--- src/unit/size.util.test.ts
+++ src/unit/size.util.test.ts
@@ -1,8 +1,13 @@
-import { _hb } from './size.util'
+import { _gb, _hb, _kb, _mb } from './size.util'
 
 test('hb', () => {
   expect(_hb()).toBe('0 byte(s)')
   expect(_hb(0)).toBe('0 byte(s)')
   expect(_hb(500)).toBe('500 byte(s)')
   expect(_hb(1000)).toBe('1 Kb')
+  expect(_hb(1024 ** 2)).toBe('1 Mb')
+  expect(_hb(1024 ** 3)).toBe('1 Gb')
+  expect(_kb(1000)).toBe(1)
+  expect(_mb(1024 ** 2)).toBe(1)
+  expect(_gb(1024 ** 3)).toBe(1)
 })
