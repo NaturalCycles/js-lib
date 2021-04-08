b95ffbc8fcbb9ac4aebf48d21e27ef7027fd2c00
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2021-04-08 19:23:59 +0200

    feat: stylelint bikeshedding properties
    'length-zero-no-unit': true
    'color-hex-length': 'short'

--- cfg/stylelint.config.js
+++ cfg/stylelint.config.js
@@ -2,5 +2,11 @@ module.exports = {
   extends: ['stylelint-config-recommended-scss'],
   rules: {
     'no-empty-source': null,
+    'length-zero-no-unit': true,
+    'color-hex-length': 'short',
+    // Prettier covers these rules already:
+    // 'color-hex-case': 'lower',
+    // 'number-leading-zero': 'always',
+    // 'number-no-trailing-zeros': true,
   },
 }
