9a0ba384ba36ba2ea93496f767a8e06178ef19f5
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2021-05-31 11:07:27 +0200

    fix: improve pProps typings
    Back-ported from original `p-props`.
    This triggered importing `type-fest` types into js-lib repo.

--- src/promise/pProps.test.ts
+++ src/promise/pProps.test.ts
@@ -1,16 +1,24 @@
+import { pDefer } from './pDefer'
 import { pDelay } from './pDelay'
 import { pProps } from './pProps'
 
 test('main', async () => {
+  const defer = pDefer<number>()
+
   const input = {
     foo: pDelay(100).then(() => 1),
     bar: Promise.resolve(2),
+    bar2: defer,
     faz: 3,
   }
+
+  setTimeout(() => defer.resolve(123), 0)
+
   const r = await pProps(input)
   expect(r).toEqual({
     foo: 1,
     bar: 2,
+    bar2: 123,
     faz: 3,
   })
 })
--- src/promise/pProps.ts
+++ src/promise/pProps.ts
@@ -8,20 +8,21 @@ Improvements:
 - Included Typescript typings (no need for @types/p-props)
  */
 
+import { PromiseValue } from '../type-fest/promise-value'
 import { pMap, PMapOptions } from './pMap'
 
 /**
  * Promise.all for Object instead of Array.
  * Supports concurrency.
  */
-export async function pProps<T>(
-  input: { [K in keyof T]: T[K] | Promise<T[K]> },
+export async function pProps<T extends Record<string, any>>(
+  input: T,
   opt?: PMapOptions,
-): Promise<T> {
-  const keys = Object.keys(input)
+): Promise<{ [key in keyof T]: PromiseValue<T[key]> }> {
+  const keys = Object.keys(input) as (keyof T)[]
   const values = await pMap(Object.values(input), r => r, opt)
 
-  const r = {} as T
+  const r = {} as { [key in keyof T]: PromiseValue<T[key]> }
   values.forEach((v, i) => {
     r[keys[i]!] = v
   })
