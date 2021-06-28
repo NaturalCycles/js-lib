4dcb1b92945fe4be616a2b8b43c5671178e27480
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2021-06-28 23:58:16 +0200

    fix: running lint-staged via node api, not via execa
    To hopefully fix "Current directory is not a git directory!"

--- src/cmd/lint-staged-def.command.ts
+++ src/cmd/lint-staged-def.command.ts
@@ -1,4 +1,3 @@
-import { execWithArgs } from '@naturalcycles/nodejs-lib/dist/exec'
 import * as fs from 'fs'
 import { cfgDir } from '../cnst/paths.cnst'
 
@@ -8,5 +7,9 @@ export async function lintStagedDefCommand(): Promise<void> {
   const sharedConfig = `${cfgDir}/lint-staged.config.js`
   const config = fs.existsSync(localConfig) ? localConfig : sharedConfig
 
-  await execWithArgs(`lint-staged`, [`--config`, config])
+  // await execWithArgs(`lint-staged`, [`--config`, config])
+  const lintStaged = require('lint-staged')
+  await lintStaged({
+    configPath: config,
+  })
 }
