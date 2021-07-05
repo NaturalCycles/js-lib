587ed1d83f5070598d6df868e1df14daa49efcfe
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2021-07-05 02:15:01 +0200

    fix: fail on `lint-staged` failure

--- src/bin/lint-staged-def.ts
+++ src/bin/lint-staged-def.ts
@@ -1,6 +1,20 @@
 #!/usr/bin/env node
 
 import { runScript } from '@naturalcycles/nodejs-lib/dist/script'
-import { lintStagedDefCommand } from '../cmd/lint-staged-def.command'
+import * as fs from 'fs'
+import { cfgDir } from '../cnst/paths.cnst'
 
-runScript(lintStagedDefCommand)
+runScript(async () => {
+  // const cwd = process.cwd()
+  const localConfig = `./lint-staged.config.js`
+  const sharedConfig = `${cfgDir}/lint-staged.config.js`
+  const config = fs.existsSync(localConfig) ? localConfig : sharedConfig
+
+  // await execWithArgs(`lint-staged`, [`--config`, config])
+  const lintStaged = require('lint-staged')
+  const success = await lintStaged({
+    configPath: config,
+  })
+
+  if (!success) process.exit(3)
+})
