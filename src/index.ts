8d800d84c9f4c4ebd33e2fdd1d85d679c66d8c32
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2021-11-17 12:46:03 +0100

    refactor: use `export *` syntax
    It works fine, except for Types, which are still exported in the old way

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,6 @@
-import { buildProdCommand } from './cmd/build-prod.command'
+export * from './cmd/build-prod.command'
 import type { BuildInfo } from './util/buildInfo.model'
-import { generateBuildInfo } from './util/buildInfo.util'
-import {
-  getLastGitCommitMsg,
-  gitCommitAll,
-  gitCurrentCommitSha,
-  gitCurrentCommitTimestamp,
-  gitCurrentBranchName,
-  gitCurrentRepoName,
-  commitMessageToTitleMessage,
-  gitIsAhead,
-  gitHasUncommittedChanges,
-  gitPush,
-  gitPull,
-} from './util/git.util'
+export * from './util/buildInfo.util'
+export * from './util/git.util'
 
-export {
-  generateBuildInfo,
-  buildProdCommand,
-  getLastGitCommitMsg,
-  gitCommitAll,
-  gitCurrentCommitSha,
-  gitCurrentCommitTimestamp,
-  gitCurrentBranchName,
-  gitCurrentRepoName,
-  commitMessageToTitleMessage,
-  gitIsAhead,
-  gitHasUncommittedChanges,
-  gitPush,
-  gitPull,
-}
 export type { BuildInfo }
--- src/testing/index.ts
+++ src/testing/index.ts
@@ -1,18 +1,8 @@
 import * as timekeeper from 'timekeeper'
-import { jestOffline } from '../jestOffline.util'
-import { expectResults } from './expect.util'
-export { mockAllKindsOfThings } from './mockAllKindsOfThings'
-import { deepFreeze, silentConsole } from './testing.util'
-import { mockTime, mockTimeMillis, MOCK_TS_2018_06_21, resetTime } from './time.util'
+export * from '../jestOffline.util'
+export * from './expect.util'
+export * from './mockAllKindsOfThings'
+export * from './testing.util'
+export * from './time.util'
 
-export {
-  jestOffline,
-  deepFreeze,
-  silentConsole,
-  mockTime,
-  mockTimeMillis,
-  resetTime,
-  MOCK_TS_2018_06_21,
-  timekeeper,
-  expectResults,
-}
+export { timekeeper }
