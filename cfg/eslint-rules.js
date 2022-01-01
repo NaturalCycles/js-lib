34ae3dfb0c48d41b59b17c9db2e546822f42ccc2
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2022-01-01 22:08:27 +0100

    fix(eslint): disable `unicorn/prefer-json-parse-buffer`
    as typescript doesn't allow it

--- cfg/eslint-rules.js
+++ cfg/eslint-rules.js
@@ -371,5 +371,6 @@ module.exports = {
     '@typescript-eslint/no-unsafe-argument': 0, // prevents "legit" use of `any`
     'unicorn/prefer-export-from': 0, // breaks auto-imports in IntelliJ Idea
     'unicorn/no-await-expression-member': 0, // some cases are better as-is
+    'unicorn/prefer-json-parse-buffer': 0, // typescript doesn't allow it
   },
 }
