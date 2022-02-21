d104c200bd06c57d98402d3e169717c527b3e7e8
Juraj Orszag <orszagj@centrum.sk>
2022-02-21 10:14:14 +0100

    fix: update length-zero-no-unit stylelint rule (#16)

--- cfg/stylelint.config.js
+++ cfg/stylelint.config.js
@@ -2,8 +2,12 @@ module.exports = {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier'],
   rules: {
     'no-empty-source': null,
-    'length-zero-no-unit': true,
     'color-hex-length': 'short',
+    'length-zero-no-unit': [
+      true,
+      // keep units in css variables because their absence breaks css calculations
+      { ignore: ['custom-properties'] },
+    ],
     // Prettier covers these rules already:
     // 'color-hex-case': 'lower',
     // 'number-leading-zero': 'always',
