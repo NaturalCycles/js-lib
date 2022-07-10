93864f6d91331e9d8f822bbc5930592ae68b0266
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2022-07-10 14:06:14 +0200

    feat: vendor `readingTime()`

---
+++ src/string/readingTime.test.ts
@@ -0,0 +1,212 @@
+import { AnyFunction } from '../types'
+import { readingTime, ReadingTimeOptions, ReadingTimeResult } from './readingTime'
+
+function runTest(
+  words: number | string,
+  exp: Partial<ReadingTimeResult>,
+  options?: ReadingTimeOptions,
+) {
+  return (done: AnyFunction) => {
+    const text = 'number' === typeof words ? generateText(words) : words
+
+    if ('string' === typeof words) {
+      if (text.includes(' ')) {
+        words = words.split(/.+ +.+/g).length + 1
+      } else if (text.length > 0) {
+        words = 1
+      } else {
+        words = 0
+      }
+    }
+
+    const res = readingTime(text, options)
+
+    if (exp.minutes) {
+      expect(res.minutes).toBe(exp.minutes)
+    }
+    if (exp.time) {
+      expect(res.time).toBe(exp.time)
+    }
+    if (exp.words) {
+      expect(res.words).toEqual(exp.words)
+    }
+    done()
+  }
+}
+
+function generateText(words: number) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789àâéèêôùûçÀÂÉÈÔÙÛÇ'
+  const charsLength = chars.length
+  let text = ''
+
+  for (let i = 0; i < words; i++) {
+    const wordLength = Math.ceil(Math.random() * 10)
+    for (let j = 0; j < wordLength; j++) {
+      text += chars[Math.floor(Math.random() * charsLength)]
+    }
+    text += ' '
+  }
+
+  return text
+}
+
+describe('readingTime()', () => {
+  test(
+    'should handle less than 1 minute text',
+    runTest(2, {
+      minutes: 1,
+      time: 600,
+    }),
+  )
+
+  test(
+    'should handle less than 1 minute text',
+    runTest(50, {
+      minutes: 1,
+      time: 15000,
+    }),
+  )
+
+  test(
+    'should handle 1 minute text',
+    runTest(100, {
+      minutes: 1,
+      time: 30000,
+    }),
+  )
+
+  test(
+    'should handle 2 minutes text',
+    runTest(300, {
+      minutes: 2,
+      time: 90000,
+    }),
+  )
+
+  test(
+    'should handle a very long text',
+    runTest(500, {
+      minutes: 3,
+      time: 150000,
+    }),
+  )
+
+  test(
+    'should handle text containing multiple successive whitespaces',
+    runTest('word  word    word', {
+      minutes: 1,
+      time: 900,
+    }),
+  )
+
+  test(
+    'should handle text starting with whitespaces',
+    runTest('   word word word', {
+      minutes: 1,
+      time: 900,
+    }),
+  )
+
+  test(
+    'should handle text ending with whitespaces',
+    runTest('word word word   ', {
+      minutes: 1,
+      time: 900,
+    }),
+  )
+
+  test(
+    'should handle text containing links',
+    runTest('word http://ngryman.sh word', {
+      minutes: 1,
+      time: 900,
+    }),
+  )
+
+  test(
+    'should handle text containing markdown links',
+    runTest('word [blog](http://ngryman.sh) word', {
+      minutes: 1,
+      time: 900,
+    }),
+  )
+
+  test(
+    'should handle text containing one word correctly',
+    runTest('0', {
+      minutes: 1,
+      time: 300,
+    }),
+  )
+
+  test(
+    'should handle text containing a black hole',
+    runTest('', {
+      minutes: 0,
+      time: 0,
+    }),
+  )
+
+  test(
+    'should accept a custom word per minutes value',
+    runTest(
+      200,
+      {
+        minutes: 2,
+        time: 120000,
+      },
+      { wordsPerMinute: 100 },
+    ),
+  )
+})
+
+describe('readingTime CJK', () => {
+  test(
+    'should handle a CJK paragraph',
+    runTest('今天，我要说中文！（没错，现在这个库也完全支持中文了）', {
+      words: { total: 22 },
+    }),
+  )
+
+  test(
+    'should handle a CJK paragraph with Latin words',
+    runTest('你会说English吗？', {
+      words: { total: 5 },
+    }),
+  )
+
+  test(
+    'should handle a CJK paragraph with Latin punctuation',
+    runTest('科学文章中, 经常使用英语标点... (虽然这段话并不科学)', {
+      words: { total: 22 },
+    }),
+  )
+
+  test(
+    'should handle a CJK paragraph starting and terminating in Latin words',
+    runTest('JoshCena喜欢GitHub', {
+      words: { total: 4 },
+    }),
+  )
+
+  test(
+    'should handle a typical Korean paragraph',
+    runTest('이것은 한국어 단락입니다', {
+      words: { total: 11 },
+    }),
+  )
+
+  test(
+    'should handle a typical Japanese paragraph',
+    runTest('天気がいいから、散歩しましょう', {
+      words: { total: 14 },
+    }),
+  )
+
+  test(
+    'should treat Katakana as one word',
+    runTest('メガナイトありませんか？', {
+      words: { total: 7 },
+    }),
+  )
+})
---
+++ src/string/readingTime.ts
@@ -0,0 +1,138 @@
+/*
+
+Vendored from https://github.com/ngryman/reading-time
+(c) Nicolas Gryman <ngryman@gmail.com>
+
+Reasons:
+1. latest (1.5.0) version had a typescript import bug.
+2. Streaming mode is not needed (but brings confusion when used in the Browser).
+
+ */
+
+export interface ReadingTimeOptions {
+  wordBound?: (char: string) => boolean
+  wordsPerMinute?: number
+}
+
+export interface ReadingTimeStats {
+  time: number
+  minutes: number
+}
+
+export interface WordCountStats {
+  total: number
+}
+
+export interface ReadingTimeResult extends ReadingTimeStats {
+  words: WordCountStats
+}
+
+type WordBoundFunction = (char: string) => boolean
+
+function codeIsInRanges(num: number, arrayOfRanges: number[][]) {
+  return arrayOfRanges.some(([lowerBound, upperBound]) => lowerBound! <= num && num <= upperBound!)
+}
+
+const isCJK: WordBoundFunction = c => {
+  const charCode = c.codePointAt(0)!
+  // Help wanted!
+  // This should be good for most cases, but if you find it unsatisfactory
+  // (e.g. some other language where each character should be standalone words),
+  // contributions welcome!
+  return codeIsInRanges(charCode, [
+    // Hiragana (Katakana not included on purpose,
+    // context: https://github.com/ngryman/reading-time/pull/35#issuecomment-853364526)
+    // If you think Katakana should be included and have solid reasons, improvement is welcomed
+    [0x3040, 0x309f],
+    // CJK Unified ideographs
+    [0x4e00, 0x9fff],
+    // Hangul
+    [0xac00, 0xd7a3],
+    // CJK extensions
+    [0x20000, 0x2ebe0],
+  ])
+}
+
+const isAnsiWordBound: WordBoundFunction = c => {
+  return ' \n\r\t'.includes(c)
+}
+
+const isPunctuation: WordBoundFunction = c => {
+  const charCode = c.codePointAt(0)!
+  return codeIsInRanges(charCode, [
+    [0x21, 0x2f],
+    [0x3a, 0x40],
+    [0x5b, 0x60],
+    [0x7b, 0x7e],
+    // CJK Symbols and Punctuation
+    [0x3000, 0x303f],
+    // Full-width ASCII punctuation variants
+    [0xff00, 0xffef],
+  ])
+}
+
+function countWords(text: string, options: ReadingTimeOptions = {}): WordCountStats {
+  let words = 0
+  let start = 0
+  let end = text.length - 1
+  const isWordBound = options.wordBound || isAnsiWordBound
+
+  // fetch bounds
+  while (isWordBound(text[start]!)) start++
+  while (isWordBound(text[end]!)) end--
+
+  // Add a trailing word bound to make handling edges more convenient
+  const normalizedText = `${text}\n`
+
+  // calculate the number of words
+  for (let i = start; i <= end; i++) {
+    // A CJK character is a always word;
+    // A non-word bound followed by a word bound / CJK is the end of a word.
+    if (
+      isCJK(normalizedText[i]!) ||
+      (!isWordBound(normalizedText[i]!) &&
+        (isWordBound(normalizedText[i + 1]!) || isCJK(normalizedText[i + 1]!)))
+    ) {
+      words++
+    }
+    // In case of CJK followed by punctuations, those characters have to be eaten as well
+    if (isCJK(normalizedText[i]!)) {
+      while (
+        i <= end &&
+        (isPunctuation(normalizedText[i + 1]!) || isWordBound(normalizedText[i + 1]!))
+      ) {
+        i++
+      }
+    }
+  }
+  return { total: words }
+}
+
+function readingTimeWithCount(
+  words: WordCountStats,
+  options: ReadingTimeOptions = {},
+): ReadingTimeStats {
+  const { wordsPerMinute = 200 } = options
+  // reading time stats
+  const minutes = words.total / wordsPerMinute
+  // Math.round used to resolve floating point funkiness
+  //   http://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
+  const time = Math.round(minutes * 60 * 1000)
+  const displayed = Math.ceil(parseFloat(minutes.toFixed(2)))
+
+  return {
+    minutes: displayed,
+    time,
+  }
+}
+
+/**
+ * API: https://github.com/ngryman/reading-time
+ */
+export function readingTime(text: string, options: ReadingTimeOptions = {}): ReadingTimeResult {
+  const words = countWords(text, options)
+  return {
+    ...readingTimeWithCount(words, options),
+    words,
+  }
+}
