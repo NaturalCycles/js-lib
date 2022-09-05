b73ade81a072edbebc6810c52f948444d734cadf
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2022-09-05 16:27:18 +0200

    refactor: use `export * from` in `index.ts`
    Because looks like WebStorm issue was fixed and these
    exports are now properly visible.

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,14 @@ export * from './array/array.util'
 export * from './lazy'
 export * from './string/url.util'
 export * from './array/range'
-import type { PromiseDecoratorCfg, PromiseDecoratorResp } from './decorators/createPromiseDecorator'
-import { _createPromiseDecorator } from './decorators/createPromiseDecorator'
+export * from './decorators/createPromiseDecorator'
 export * from './decorators/debounce'
 export * from './decorators/debounce.decorator'
 export * from './decorators/decorator.util'
 export * from './decorators/logMethod.decorator'
 export * from './decorators/memo.decorator'
 export * from './decorators/asyncMemo.decorator'
-import type { MemoCache, AsyncMemoCache } from './decorators/memo.util'
+export * from './decorators/memo.util'
 export * from './decorators/memoFn'
 export * from './decorators/memoFnAsync'
 export * from './decorators/retry.decorator'
@@ -18,44 +17,18 @@ export * from './decorators/timeout.decorator'
 export * from './error/app.error'
 export * from './error/assert'
 export * from './enum.util'
-import type {
-  Admin401ErrorData,
-  Admin403ErrorData,
-  ErrorData,
-  ErrorObject,
-  HttpErrorData,
-  HttpErrorResponse,
-} from './error/error.model'
+export * from './error/error.model'
 export * from './error/error.util'
-import { ErrorMode } from './error/errorMode'
+export * from './error/errorMode'
 export * from './error/http.error'
 export * from './error/try'
-import type { TryCatchOptions } from './error/tryCatch'
-import { _TryCatch, _tryCatch } from './error/tryCatch'
+export * from './error/tryCatch'
 export * from './json-schema/from-data/generateJsonSchemaFromData'
 export * from './json-schema/jsonSchema.cnst'
-import type {
-  JsonSchema,
-  JsonSchemaAllOf,
-  JsonSchemaAny,
-  JsonSchemaAnyOf,
-  JsonSchemaArray,
-  JsonSchemaBoolean,
-  JsonSchemaConst,
-  JsonSchemaEnum,
-  JsonSchemaNot,
-  JsonSchemaNull,
-  JsonSchemaNumber,
-  JsonSchemaRootObject,
-  JsonSchemaObject,
-  JsonSchemaOneOf,
-  JsonSchemaRef,
-  JsonSchemaString,
-  JsonSchemaTuple,
-} from './json-schema/jsonSchema.model'
+export * from './json-schema/jsonSchema.model'
 export * from './json-schema/jsonSchema.util'
-import type { JsonSchemaBuilder } from './json-schema/jsonSchemaBuilder'
-import { jsonSchema, JsonSchemaAnyBuilder } from './json-schema/jsonSchemaBuilder'
+export * from './json-schema/jsonSchemaBuilder'
+export * from './json-schema/jsonSchemaBuilder'
 export * from './math/math.util'
 export * from './math/sma'
 export * from './number/createDeterministicRandom'
@@ -64,95 +37,31 @@ export * from './object/deepEquals'
 export * from './object/object.util'
 export * from './object/sortObject'
 export * from './object/sortObjectDeep'
-import { AggregatedError } from './promise/AggregatedError'
-import type { DeferredPromise } from './promise/pDefer'
-import { pDefer } from './promise/pDefer'
+export * from './promise/AggregatedError'
+export * from './promise/pDefer'
 export * from './promise/pDelay'
 export * from './promise/pFilter'
 export * from './promise/pHang'
-import type { PMapOptions } from './promise/pMap'
-import { pMap } from './promise/pMap'
+export * from './promise/pMap'
 export * from './promise/pProps'
-import type { PRetryOptions } from './promise/pRetry'
-import { pRetry, pRetryFn } from './promise/pRetry'
+export * from './promise/pRetry'
 export * from './promise/pState'
-import type { PTimeoutOptions } from './promise/pTimeout'
-import { pTimeout, pTimeoutFn } from './promise/pTimeout'
+export * from './promise/pTimeout'
 export * from './string/case'
 export * from './string/json.util'
 export * from './string/string.util'
 export * from './string/readingTime'
 export * from './string/escape'
 export * from './string/pupa'
-import type { JsonStringifyFunction, StringifyAnyOptions } from './string/stringifyAny'
-import { _stringifyAny } from './string/stringifyAny'
+export * from './string/stringifyAny'
 export * from './time/time.util'
 export * from './is.util'
-
-import type {
-  Class,
-  ConditionalExcept,
-  ConditionalPick,
-  Merge,
-  Promisable,
-  ReadonlyDeep,
-  Simplify,
-} from './typeFest'
-import type {
-  AsyncMapper,
-  AsyncPredicate,
-  BaseDBEntity,
-  CreatedUpdated,
-  CreatedUpdatedId,
-  ObjectWithId,
-  AnyObjectWithId,
-  Saved,
-  Unsaved,
-  UnsavedId,
-  BatchResult,
-  InstanceId,
-  IsoDate,
-  IsoDateString,
-  IsoDateTimeString,
-  KeyValueTuple,
-  Mapper,
-  ObjectMapper,
-  ObjectPredicate,
-  Predicate,
-  PromiseMap,
-  AnyObject,
-  AnyEnum,
-  NumberEnum,
-  StringEnum,
-  AnyFunction,
-  Reviver,
-  SavedDBEntity,
-  StringMap,
-  UnixTimestampNumber,
-  UnixTimestampMillisNumber,
-  UnixTimestamp,
-  Integer,
-  ValueOf,
-  ValuesOf,
-  AbortableMapper,
-  AbortableAsyncPredicate,
-  AbortableAsyncMapper,
-  AbortablePredicate,
-  NullishValue,
-  FalsyValue,
-} from './types'
+export * from './typeFest'
 export * from './types'
 export * from './unit/size.util'
 import { is } from './vendor/is'
-import type {
-  CommonLogLevel,
-  CommonLogFunction,
-  CommonLogger,
-  CommonLogWithLevelFunction,
-} from './log/commonLogger'
 export * from './log/commonLogger'
 export * from './string/safeJsonStringify'
-import type { PQueueCfg } from './promise/pQueue'
 export * from './promise/pQueue'
 export * from './seq/seq'
 export * from './math/stack.util'
@@ -161,142 +70,9 @@ export * from './datetime/localDate'
 export * from './datetime/localTime'
 export * from './datetime/dateInterval'
 export * from './datetime/timeInterval'
-import type {
-  LocalDateConfig,
-  LocalDateFormatter,
-  LocalDateUnit,
-  LocalDateUnitStrict,
-  Inclusiveness,
-} from './datetime/localDate'
-import type {
-  LocalTimeConfig,
-  LocalTimeFormatter,
-  LocalTimeUnit,
-  LocalTimeComponents,
-  ISODayOfWeek,
-} from './datetime/localTime'
-import type { DateIntervalConfig, DateIntervalString } from './datetime/dateInterval'
-import type { TimeIntervalConfig, TimeIntervalString } from './datetime/timeInterval'
+export * from './datetime/localDate'
+export * from './datetime/localTime'
+export * from './datetime/dateInterval'
+export * from './datetime/timeInterval'
 
-export type {
-  DateIntervalConfig,
-  DateIntervalString,
-  TimeIntervalConfig,
-  TimeIntervalString,
-  LocalDateConfig,
-  LocalDateFormatter,
-  LocalDateUnit,
-  LocalDateUnitStrict,
-  Inclusiveness,
-  LocalTimeConfig,
-  LocalTimeFormatter,
-  LocalTimeUnit,
-  ISODayOfWeek,
-  LocalTimeComponents,
-  AbortableMapper,
-  AbortablePredicate,
-  AbortableAsyncPredicate,
-  AbortableAsyncMapper,
-  PQueueCfg,
-  MemoCache,
-  AsyncMemoCache,
-  PromiseDecoratorCfg,
-  PromiseDecoratorResp,
-  ErrorData,
-  ErrorObject,
-  HttpErrorData,
-  HttpErrorResponse,
-  Admin401ErrorData,
-  Admin403ErrorData,
-  StringMap,
-  PromiseMap,
-  AnyObject,
-  AnyEnum,
-  NumberEnum,
-  StringEnum,
-  AnyFunction,
-  ValuesOf,
-  ValueOf,
-  KeyValueTuple,
-  ObjectMapper,
-  ObjectPredicate,
-  InstanceId,
-  IsoDate,
-  IsoDateString,
-  IsoDateTimeString,
-  Reviver,
-  FalsyValue,
-  NullishValue,
-  PMapOptions,
-  Mapper,
-  AsyncMapper,
-  Predicate,
-  AsyncPredicate,
-  BatchResult,
-  DeferredPromise,
-  PRetryOptions,
-  PTimeoutOptions,
-  TryCatchOptions,
-  StringifyAnyOptions,
-  JsonStringifyFunction,
-  Merge,
-  ReadonlyDeep,
-  Promisable,
-  Simplify,
-  ConditionalPick,
-  ConditionalExcept,
-  Class,
-  UnixTimestampNumber,
-  UnixTimestampMillisNumber,
-  UnixTimestamp,
-  Integer,
-  BaseDBEntity,
-  SavedDBEntity,
-  Saved,
-  Unsaved,
-  UnsavedId,
-  CreatedUpdated,
-  CreatedUpdatedId,
-  ObjectWithId,
-  AnyObjectWithId,
-  JsonSchema,
-  JsonSchemaAny,
-  JsonSchemaOneOf,
-  JsonSchemaAllOf,
-  JsonSchemaAnyOf,
-  JsonSchemaNot,
-  JsonSchemaRef,
-  JsonSchemaConst,
-  JsonSchemaEnum,
-  JsonSchemaString,
-  JsonSchemaNumber,
-  JsonSchemaBoolean,
-  JsonSchemaNull,
-  JsonSchemaRootObject,
-  JsonSchemaObject,
-  JsonSchemaArray,
-  JsonSchemaTuple,
-  JsonSchemaBuilder,
-  CommonLogLevel,
-  CommonLogWithLevelFunction,
-  CommonLogFunction,
-  CommonLogger,
-}
-
-export {
-  is,
-  _createPromiseDecorator,
-  pMap,
-  ErrorMode,
-  pDefer,
-  AggregatedError,
-  pRetry,
-  pRetryFn,
-  pTimeout,
-  pTimeoutFn,
-  _tryCatch,
-  _TryCatch,
-  _stringifyAny,
-  jsonSchema,
-  JsonSchemaAnyBuilder,
-}
+export { is }
