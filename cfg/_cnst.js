b7e126666f6b5a4395c40b4f6d354204d0459050
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2022-10-26 00:18:54 +0200

    feat: add `blocks` to prettierDirs
    It's a common root folder in BEM projects

--- cfg/_cnst.js
+++ cfg/_cnst.js
@@ -1,6 +1,6 @@
 module.exports = {
   prettierDirs: [
-    'src,scripts,e2e,docs,cfg,resources,.circleci,.github,public,static,components,content,layouts,pages,plugins,middleware,store',
+    'src,scripts,e2e,docs,cfg,resources,.circleci,.github,public,static,components,content,layouts,pages,plugins,middleware,store,blocks',
   ],
   // ts,tsx,css,scss excluded, cause they need to run in special order (overlap between >1 tool):
   prettierExtensionsExclusive: 'js,jsx,json,md,graphql,yml,yaml,html',
