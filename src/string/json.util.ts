7c85fdab2a4c24b4853f08de36e5cf5de7ebb11c
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2023-05-21 22:40:56 +0200

    fix: types

--- src/string/json.util.ts
+++ src/string/json.util.ts
@@ -8,7 +8,7 @@ const DETECT_JSON = /^\s*[{["\-\d]/
  * Attempts to parse object as JSON.
  * Returns original object if JSON parse failed (silently).
  */
-export function _jsonParseIfPossible(obj: any, reviver?: Reviver): unknown {
+export function _jsonParseIfPossible(obj: any, reviver?: Reviver): any {
   // Optimization: only try to parse if it looks like JSON: starts with a json possible character
   if (typeof obj === 'string' && obj && DETECT_JSON.test(obj)) {
     try {
