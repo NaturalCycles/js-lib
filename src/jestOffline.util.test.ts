c5da91e3e04eeb799b3d8e18437207600425e983
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2023-08-24 21:46:34 +0200

    chore: deps

--- src/jestOffline.util.test.ts
+++ src/jestOffline.util.test.ts
@@ -1,6 +1,4 @@
 import { _stringifyAny, getFetcher, pExpectedError } from '@naturalcycles/js-lib'
-import { getGot } from '@naturalcycles/nodejs-lib'
-const got = getGot()
 const fetcher = getFetcher({
   retry: { count: 0 },
 })
@@ -9,7 +7,6 @@ const detectLeaks = process.argv.some(a => a.includes('detectLeaks'))
 
 test('should throw on network connections', async () => {
   if (detectLeaks) return // skip test on detectLeaks where jestOffline is disabled
-  await expect(got('http://example.com')).rejects.toThrow('Network request forbidden')
 
   const err = await pExpectedError(fetcher.get('http://example.com'))
   expect(_stringifyAny(err)).toMatchInlineSnapshot(`
@@ -20,8 +17,5 @@ test('should throw on network connections', async () => {
 })
 
 test('should allow connection to local hosts', async () => {
-  await got('http://127.0.0.1', { timeout: 1, retry: 0 }).catch(_ => {})
-  await got('http://localhost', { timeout: 1, retry: 0 }).catch(_ => {})
-
   await fetcher.get('http://localhost').catch(_ => {})
 }, 20_000)
