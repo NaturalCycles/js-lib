c1d0ed8fcac99030582e1f8f52a7f5bac96fa347
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2024-01-19 11:25:20 +0100

    feat: AnyPartialObjectWithId

--- src/types.ts
+++ src/types.ts
@@ -39,6 +39,8 @@ export type PartialObjectWithId = {
   id?: string
 }
 
+export interface AnyPartialObjectWithId extends AnyObject, PartialObjectWithId {}
+
 export interface AnyObjectWithId extends AnyObject, ObjectWithId {}
 
 /**
