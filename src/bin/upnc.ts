67014433eab476edfe665fd260c6e4e73248353b
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2024-01-21 20:17:06 +0100

    fix: run yarn-deduplicate after `yarn nc`

--- src/bin/upnc.ts
+++ src/bin/upnc.ts
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 
-import fs from 'node:fs'
 import { execVoidCommandSync, runScript } from '@naturalcycles/nodejs-lib'
 
 runScript(async () => {
   execVoidCommandSync('yarn', ['upgrade', '--pattern', `@naturalcycles`])
-
-  if (fs.existsSync(`node_modules/patch-package`)) {
-    execVoidCommandSync('patch-package')
-  }
+  execVoidCommandSync('yarn-deduplicate')
+  execVoidCommandSync('yarn')
 })
