0a81942a86b7376bed9d18d532c772029965b353
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2024-04-06 12:51:45 +0200

    chore: eslintPrintConfig to track changes in eslint output config

---
+++ scripts/eslintPrintConfig.script.ts
@@ -0,0 +1,25 @@
+/*
+
+yarn tsn eslintPrintConfig
+
+This script allows to track changes in the final eslint config output,
+like a "manual snapshot test".
+Changes are visible in git diff every time they are observed.
+
+ */
+
+import { _substringAfter } from '@naturalcycles/js-lib'
+import { execVoidCommandSync, fs2, runScript } from '@naturalcycles/nodejs-lib'
+import { testDir } from '../src/cnst/paths.cnst'
+
+runScript(async () => {
+  const outputPath = `${testDir}/cfg/eslint.config.json`
+
+  execVoidCommandSync(`eslint --print-config src/index.ts > ${outputPath}`, [], {
+    shell: true,
+  })
+
+  const r = fs2.readJson<any>(outputPath)
+  r.parser = _substringAfter(r.parser, 'dev-lib/')
+  fs2.writeJson(outputPath, r, { spaces: 2 })
+})
--- src/cnst/paths.cnst.ts
+++ src/cnst/paths.cnst.ts
@@ -2,6 +2,7 @@ import path from 'node:path'
 
 export const projectDir = path.join(__dirname, '../..')
 export const srcDir = `${projectDir}/src`
+export const testDir = `${srcDir}/test`
 export const cfgDir = `${projectDir}/cfg`
 export const cfgOverwriteDir = `${projectDir}/cfg/overwrite`
 export const scriptsDir = `${projectDir}/scripts`
