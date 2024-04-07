f171d1cc2e81b99a60e45ad3f4596db51c4d415d
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2024-04-07 12:15:16 +0200

    fix(eslint): disable buggy `unicorn/no-array-method-this-argument`

--- cfg/eslint-rules.js
+++ cfg/eslint-rules.js
@@ -354,6 +354,7 @@ module.exports = {
     'unicorn/no-array-push-push': 0,
     'unicorn/no-abusive-eslint-disable': 0,
     'unicorn/no-negated-condition': 0,
+    'unicorn/no-array-method-this-argument': 0, // bug: wrongly removes`readable.flatMap` concurrency option
     'unicorn/number-literal-case': 0, // conflicts with prettier
     'unicorn/prevent-abbreviations': 0,
     'unicorn/prefer-module': 0,
