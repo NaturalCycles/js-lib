22fbfa597c8f07895c4b1ca4bbaeb7d8fa1837d4
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2024-07-21 13:50:48 +0200

    chore: deps

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+// prettier-ignore
+module.exports = [
+  ...require('@naturalcycles/dev-lib/cfg/eslint.config'),
+  {
+    rules: {
+      'unicorn/no-array-reduce': 0,
+    },
+  },
+]
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   ...require('@naturalcycles/dev-lib/cfg/jest.config'),
-  // transform: {
-  //   // example (experimental):
-  //   '^.+\\.ts$': '@naturalcycles/dev-lib/cfg/jest.esbuild.transformer.js',
-  // },
-  prettierPath: null, // todo: remove when jest has fixed it https://github.com/jestjs/jest/issues/14305
 }
