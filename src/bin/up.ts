3babad2a316dfbdbd86612b07d1713fef63aaea3
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2024-07-23 16:00:14 +0200

    feat: `yarn up` now runs `yarn-deduplicate` as well
    Similar to `upnc`.

--- src/bin/up.ts
+++ src/bin/up.ts
@@ -5,6 +5,8 @@ import { execVoidCommandSync, runScript } from '@naturalcycles/nodejs-lib'
 
 runScript(async () => {
   execVoidCommandSync('yarn', ['upgrade'])
+  execVoidCommandSync('yarn-deduplicate')
+  execVoidCommandSync('yarn')
 
   if (fs.existsSync(`node_modules/patch-package`)) {
     execVoidCommandSync('patch-package')
