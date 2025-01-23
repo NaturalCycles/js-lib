0c31039d4ddde0bed1b5445b13c9e7675cee0492
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2025-01-23 17:17:23 +0100

    feat: exclude more bots

--- src/bot.ts
+++ src/bot.ts
@@ -20,7 +20,7 @@ export interface BotDetectionServiceCfg {
 }
 
 const botRegex =
-  /bot|spider|crawl|headless|electron|phantom|slimer|proximic|cincraw|snapchat|slurp|MicrosoftPreview/i
+  /bot|spider|crawl|headless|electron|phantom|slimer|proximic|cincraw|snapchat|slurp|MicrosoftPreview|ahrefs|preview|lighthouse|facebookexternal|pinterest|screaming|apis-google|duplexweb-google|feedfetcher-google|google-read-aloud|googleweblight|mediapartners-google/i
 
 /**
  * Service to detect bots and CDP (Chrome DevTools Protocol).
