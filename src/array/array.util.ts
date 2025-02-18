d900cdd1a94ef550a82c4b627608964850b55359
Fróði Karlsson <frodikarlsson@gmail.com>
2025-02-18 18:07:17 +0100

    fix: remove default for type param

--- src/array/array.util.ts
+++ src/array/array.util.ts
@@ -347,7 +347,7 @@ export function _difference<T>(a1: T[], a2: T[] | Set<T>): T[] {
 /**
  * Returns the sum of items, or 0 for empty array.
  */
-export function _sum<N extends number = number>(items: Iterable<N>): N {
+export function _sum<N extends number>(items: Iterable<N>): N {
   let sum = 0 as N
   for (const n of items) {
     sum = (sum + n) as N
@@ -355,7 +355,7 @@ export function _sum<N extends number = number>(items: Iterable<N>): N {
   return sum
 }
 
-export function _sumBy<T, N extends number = number>(
+export function _sumBy<T, N extends number>(
   items: Iterable<T>,
   mapper: Mapper<T, N | undefined>,
 ): N {
