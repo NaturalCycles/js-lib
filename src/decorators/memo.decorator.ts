21ae4135b55bb9d09651797422e718c7c4e98438
Fróði Karlsson <53127288+frodi-karlsson@users.noreply.github.com>
2025-02-27 17:42:39 +0100

    feat: truly generic cachekey (#31)
    * feat: truly generic cachekey
    
    * fix: niche use case
    
    * fix: memoFn maybeparams
    
    * fix: cleanup
    
    * refactor: rename to MaybeParameters
    
    * fix: add back memo on non-function message

--- src/decorators/memo.decorator.ts
+++ src/decorators/memo.decorator.ts
@@ -1,11 +1,11 @@
-import { _assert } from '../error/assert'
+import { _assert, _assertTypeOf } from '../error/assert'
 import type { CommonLogger } from '../log/commonLogger'
-import { _objectAssign, AnyFunction, AnyObject } from '../types'
+import { _objectAssign, AnyFunction, AnyObject, MaybeParameters } from '../types'
 import { _getTargetMethodSignature } from './decorator.util'
 import type { MemoCache } from './memo.util'
 import { jsonMemoSerializer, MapMemoCache } from './memo.util'
 
-export interface MemoOptions<T extends AnyFunction> {
+export interface MemoOptions<T> {
   /**
    * Provide a custom implementation of MemoCache.
    * Function that creates an instance of `MemoCache`.
@@ -16,7 +16,7 @@ export interface MemoOptions<T extends AnyFunction> {
   /**
    * Provide a custom implementation of CacheKey function.
    */
-  cacheKeyFn?: (args: Parameters<T>) => any
+  cacheKeyFn?: (args: MaybeParameters<T>) => any
 
   /**
    * Default to `console`
@@ -34,6 +34,13 @@ export interface MemoInstance {
 
   getCache: (instance: AnyFunction) => MemoCache | undefined
 }
+
+// We override MethodDecorator to make it generic
+type MethodDecorator<T> = (
+  target: AnyObject,
+  propertyKey: string | symbol,
+  descriptor: TypedPropertyDescriptor<T>,
+) => TypedPropertyDescriptor<T> | undefined
 
 /**
  * Memoizes the method of the class, so it caches the output and returns the cached version if the "key"
@@ -57,11 +64,13 @@ export interface MemoInstance {
  */
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const _Memo =
-  <T extends AnyFunction>(opt: MemoOptions<T> = {}): MethodDecorator =>
+  <T>(opt: MemoOptions<T> = {}): MethodDecorator<T> =>
   (target, key, descriptor) => {
-    if (typeof descriptor.value !== 'function') {
-      throw new TypeError('Memoization can be applied only to methods')
-    }
+    _assertTypeOf<AnyFunction>(
+      descriptor.value,
+      'function',
+      'Memoization can be applied only to methods',
+    )
 
     const originalFn = descriptor.value
 
@@ -83,7 +92,7 @@ export const _Memo =
     const keyStr = String(key)
     const methodSignature = _getTargetMethodSignature(target, keyStr)
 
-    descriptor.value = function (this: typeof target, ...args: Parameters<T>): any {
+    descriptor.value = function (this: typeof target, ...args: MaybeParameters<T>): any {
       const ctx = this
       const cacheKey = cacheKeyFn(args)
 
@@ -110,7 +119,7 @@ export const _Memo =
       return value
     } as any
 
-    _objectAssign(descriptor.value as MemoInstance, {
+    _objectAssign<MemoInstance>(descriptor.value as any, {
       clear: () => {
         logger.log(`${methodSignature} @_Memo.clear()`)
         instanceCache.forEach(memoCache => memoCache.clear())
