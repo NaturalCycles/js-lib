9523928fc6990379380c178c835b7013a9da9dda
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2025-03-23 10:53:18 +0100

    fix: remove _yargs helper, it's moved to nodejs-lib instead

--- src/yargs.util.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from 'vitest'
-import { _yargs } from './yargs.util'
-
-test('_yargs', () => {
-  expect(typeof _yargs.options).toBe('function')
-})
--- src/yargs.util.ts
+++
@@ -1,7 +0,0 @@
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
-
-/**
- * Quick yargs helper to make it work in esm.
- */
-export const _yargs = yargs(hideBin(process.argv))
